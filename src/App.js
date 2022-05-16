60ee273ecc81ffe9662d59902ce079a58cb1d908
Theo Dao <trungdao912@gmail.com>
2022-05-16 11:25:13 +0700

    chore: update new lib version

--- src/App.js
+++ src/App.js
@@ -23,7 +23,19 @@ function App() {
   return (
     <>
       <SwitchApp />
-      <MainApp isOpen={isOpenApp} isEmbeded={false} onHandleClose={onCloseApp} />
+      <MainApp
+        isOpen={isOpenApp}
+        isEmbeded={false}
+        onHandleClose={onCloseApp}
+        injectedConnectorConfig={{
+          supportedChainIds: [89, 56, 88],
+        }}
+        walletConnectorConfig={{
+          rpc: { 56: 'https://solitary-snowy-river.bsc.quiknode.pro/16b4e8d1466a4e5c06c88145a2faed83b3661fd9/' },
+          bridge: 'https://bridge.walletconnect.org',
+          qrcode: true,
+        }}
+      />
     </>
   )
 }
